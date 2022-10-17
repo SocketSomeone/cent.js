ce8c35579193e35e47304f4fc4f56ad1b82b2942
SocketSomeone <socket.someone@gmail.com>
2022-10-17 19:32:57 +0300

    fix: axios request

--- src/cent.client.ts
+++ src/cent.client.ts
@@ -1,24 +1,24 @@
-import {Axios} from 'axios';
-import {CentException} from './cent.exception';
-import {CentOptions, CentParams, CentResponses} from './interfaces';
-import {CentMethods} from './cent-methods.enum';
+import { Axios } from 'axios';
+import { CentException } from './cent.exception';
+import { CentOptions, CentParams, CentResponses } from './interfaces';
+import { CentMethods } from './cent-methods.enum';
 
 export class CentClient extends Axios {
-	public constructor(options: CentOptions) {
+	public constructor(private readonly centOptions: CentOptions) {
 		super({
-			baseURL: options.url,
-			timeout: options.timeout,
+			baseURL: centOptions.url,
+			timeout: centOptions.timeout,
 			headers: {
 				'Content-Type': 'application/json',
-				Authorization: `apikey ${options.token}`
+				Authorization: `apikey ${centOptions.token}`
 			}
 		});
 	}
 
 	private methodFactory<M extends CentMethods>(method: M) {
 		return (params?: CentParams[M]): Promise<CentResponses[M]> =>
-			this.post('/', {method, params})
-				.then(({data}) => data?.result)
+			this.post('', JSON.stringify({ method, params }))
+				.then(res => JSON.parse(res.data)?.result)
 				.catch(err => {
 					throw new CentException(err);
 				});
